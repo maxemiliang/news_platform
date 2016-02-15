befd371e064edde15de084a978b06a33815d2bb4
Maxemilian Grönblom <maxemiliang@gmail.com>
2016-02-15 23:11:31 +0200

    just a small commit to change some text

--- views/home.php
+++ views/home.php
@@ -8,7 +8,7 @@ include "head.php";
       <div class="starter-template">
 
         <h1>Välkommen</h1>
-        <p class="lead">Till NEWS 24/7 Din nyhetskälla!</p>
+        <p class="lead">Till NEWS 24/7 Din nyhetskälla, för de nyaste nyheterna!</p>
       </div>
 
     </div>
